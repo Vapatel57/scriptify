84ee4cd44a67117feba33ff9af0c559d9c1fa008
Vaidikmakasana57 <2301031000037@silveroakuni.ac.in>
2025-07-13 11:16:50 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -5,10 +5,13 @@ import json
 import streamlit as st
 
 # ✅ Initialize Firebase app
+# if not firebase_admin._apps:
+#     with open("serviceAccountKey.json") as f:
+#         service_account_info = json.load(f)
+#     cred = credentials.Certificate(service_account_info)
+#     firebase_admin.initialize_app(cred)
 if not firebase_admin._apps:
-    with open("serviceAccountKey.json") as f:
-        service_account_info = json.load(f)
-    cred = credentials.Certificate(service_account_info)
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
 
 # ✅ Firestore client
